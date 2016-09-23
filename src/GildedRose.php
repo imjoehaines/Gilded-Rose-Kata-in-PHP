99511b0a449337c2c353b7ae38cdd351300fb19c
Joe Haines <hello@joehaines.co.uk>
2016-09-23 20:04:39 +0100

    Move item lookup to static property

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,16 +4,16 @@ namespace App;
 
 class GildedRose
 {
+    private static $itemLookup = [
+        'normal' => Normal::class,
+        'Aged Brie' => AgedBrie::class,
+        'Sulfuras, Hand of Ragnaros' => SulfurasHandOfRagnaros::class,
+        'Backstage passes to a TAFKAL80ETC concert' => BackstagePasses::class,
+        'Conjured Mana Cake',
+    ];
+
     public static function of(string $name, int $quality, int $sellIn) : Item
     {
-        $itemLookup = [
-            'normal' => Normal::class,
-            'Aged Brie' => AgedBrie::class,
-            'Sulfuras, Hand of Ragnaros' => SulfurasHandOfRagnaros::class,
-            'Backstage passes to a TAFKAL80ETC concert' => BackstagePasses::class,
-            'Conjured Mana Cake',
-        ];
-
-        return new $itemLookup[$name]($quality, $sellIn);
+        return new static::$itemLookup[$name]($quality, $sellIn);
     }
 }
